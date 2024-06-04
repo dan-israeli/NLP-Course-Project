ccd892991974a15022664bc31dbb50e516c39db2
Dan Israeli <81325077+dan-israeli@users.noreply.github.com>
2024-06-05 00:07:41 +0300

    Update best_new_strategy_(strictly_positive)_results_sweep.py

--- primary research/sweeps/code/best_new_strategy_(strictly_positive)_results_sweep.py
+++ primary research/sweeps/code/best_new_strategy_(strictly_positive)_results_sweep.py
@@ -30,7 +30,7 @@ sweep_config = {
     "command": command
 }
 
-# Initialize a new sweep (original)
+# Initialize a new sweep
 sweep_id = wandb.sweep(sweep=sweep_config, project=project)
 print("run this line to run your agent in a screen:")
 print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
